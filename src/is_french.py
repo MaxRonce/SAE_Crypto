f207318c3e17e69ecd1f076b93bc96c1d36aa983
MaxRonce <max.ron@outlook.fr>
2022-10-11 15:11:10 +0200

    + is_french.py + caesar.py + text_input.py

---
+++ src/is_french.py
@@ -0,0 +1,69 @@
+ALPHABET= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+letter_occurence_dict = {"A" : 7.11, "B" : 1.14, "C" : 3.18, "D" : 3.67,
+"E" : 12.10, "F" : 1.11, "G" : 1.23, "H" : 1.11, "I" : 6.59, "J" : 0.34, "K" : 0.29,
+"L" : 4.96, "M" : 2.62, "N" : 6.39, "O" : 5.02, "P" : 2.49, "Q" : 0.65, "R" : 6.07,
+"S" : 6.51, "T" : 5.92, "U" : 4.49, "V" : 1.11, "W" : 0.17, "X" : 0.32, "Y" : 0.46, "Z" : 0.15}
+
+#count occurence of each letter in the given text
+
+def count_letters(text: str) -> dict:
+    """
+    It takes a string (no punctuation and UPPER) as input and returns a dictionary with the letters of the alphabet as keys and the number of times
+    they appear in the string as values
+    :param text: The text to be counted
+    :return: A dictionary with the letters of the alphabet as keys and the number of times they appear in the string as
+    values
+    """
+    text = list(text)
+    letter_count_dict = {}
+    for letter in ALPHABET:
+        letter_count_dict[letter] = text.count(letter)
+    return letter_count_dict
+
+#calculate the percentage of each letter in the given text
+
+def calculate_percentage(text: str) -> dict:
+    """
+    It takes a string (no punctuation and UPPER) as input and returns a dictionary with the letters of the alphabet as keys and the percentage of
+    times they appear in the string as values
+    :param text: The text to be counted
+    :return: A dictionary with the letters of the alphabet as keys and the percentage of times they appear in the string as
+    values
+    """
+    letter_count_dict = count_letters(text)
+    letter_percentage_dict = {}
+    for letter in ALPHABET:
+        letter_percentage_dict[letter] = round((letter_count_dict[letter] / len(text)) * 100, 2)
+    return letter_percentage_dict
+
+#calculate the euclidean difference between the percentage of each letter in the given text and the percentage of each letter in the
+#french language
+
+def calculate_euclidean_difference(text: str) -> float:
+    """
+    It takes a string (no punctuation and UPPER) as input and returns the euclidean difference between the percentage of each letter in the given
+    text and the percentage of each letter in the french language
+    :param text: The text to be counted
+    :return: A float
+    """
+    letter_percentage_dict = calculate_percentage(text)
+    euclidean_difference = 0
+    for letter in ALPHABET:
+        euclidean_difference += (letter_percentage_dict[letter] - letter_occurence_dict[letter]) ** 2
+    return round(euclidean_difference ** 0.5, 2)
+
+#TODO function is_french that takes a list of strings and returns the string that is the most likely to be french and it's index in the list,
+#using the euclidean difference function above as a metric and the strip_punctuation function from text_input.py
+
+def is_french(text_list: list) -> tuple:
+    """
+    It takes a list of strings as input and returns the string that is the most likely to be french and it's index in the list
+    :param text_list: The list of strings to be checked
+    :return: A tuple with the index of the string that is the most likely to be french and the string itself
+    """
+    euclidean_difference_list = []
+    for text in text_list:
+        euclidean_difference_list.append(calculate_euclidean_difference(text))
+    return euclidean_difference_list.index(min(euclidean_difference_list)), text_list[euclidean_difference_list.index(min(euclidean_difference_list))]
+
---
+++ src/text_input.py
@@ -0,0 +1,59 @@
+from typing import Tuple, Dict, Any
+
+import unidecode
+import string
+
+
+def text_format(text_input : str) -> str:
+    """
+    It takes a string as input, converts it to uppercase, and then removes any accents
+
+    :param text_input: The text you want to format
+    :return: A string
+    """
+    text_input = text_input.upper()
+    try:
+        text_input = unidecode.unidecode(text_input)
+    except NameError:
+        pass
+    return str(text_input)
+
+def strip_puntuation(text: str) -> tuple[str, dict[int, Any]]:
+    """
+    It takes a string, formats it, turns it into a list, makes a copy of the list, and then creates a dictionary of the
+    punctuation and spaces in the original list. Then it removes the punctuation and spaces from the copy and returns the
+    copy as a string
+
+    :param text: the text to be stripped of punctuation
+    :type text: str
+    :return: A tuple with the first element being the string without punctuation and the second element being a dictionary
+    with the punctuation and spaces as keys and the punctuation and spaces as values.
+    """
+    text = text_format(text)
+    text = list(text)
+    text_copy = text
+    dic_text = {}
+
+    for i in range(len(text)):
+        if text[i] in string.punctuation+" ":
+            dic_text[i] = text[i]
+    for i in dic_text.values():
+        text_copy.remove(i)
+
+    return "".join(text_copy), dic_text
+
+def insert_punctuation(text: str, dic: dict) -> str:
+    """
+    It takes a string and a dictionary as input, and returns a string with punctuation inserted at the positions specified
+    in the dictionary
+
+    :param text: the text to be punctuated
+    :type text: str
+    :param dic: a dictionary of punctuation marks and their positions in the text
+    :type dic: dict
+    :return: A string with punctuation inserted at the correct locations. Uppercase
+    """
+    text = list(text)
+    for i in dic.keys():
+        text.insert(i, dic[i])
+    return "".join(text)
