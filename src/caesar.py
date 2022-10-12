9f517bed4efd30e9240daa69ca9045090572ee58
Yrow0 <anto28700@gmail.com>
2022-10-12 12:16:41 +0200

    caesar.py is working and tested file added

--- src/caesar.py
+++ src/caesar.py
@@ -1,4 +1,7 @@
 
+from text_input import strip_puntuation, insert_punctuation, text_format
+from is_french import is_french
+
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #TODO fonction qui prend en para un txt -> str sans ponctu ni espace en maj, déchiffre avec un chiffrage cesar avec une clé donnée
 
@@ -12,7 +15,7 @@ def caesar_decrypt(text: str, key: int) -> str:
     text = list(text)
     decrypted_text = []
     for letter in text:
-        decrypted_text.append(ALPHABET[(ALPHABET.index(letter) - key) % 26])
+        decrypted_text.append(ALPHABET[(ALPHABET.index(letter) + key) % 26])
     return "".join(decrypted_text)
 
 #TODO fonction qui prend en para un txt -> test toutes les possibilités de clé et renvoie une liste de str contenant tous les txt déchiffrés
@@ -23,6 +26,15 @@ def caesar_decrypt_all(text: str) -> list:
     :return: A list of strings
     """
     decrypted_text_list = []
-    for key in range(26):
+    for key in range(25):
         decrypted_text_list.append(caesar_decrypt(text, key+1))
     return decrypted_text_list
+
+if __name__ == "__main__":
+
+
+    text = "Oh kdoo g'hqwuhh gx fkdwhdx hwdlw vl judqg txh od pdlvrq ghv Gxuvohb dxudlw sx b whqlu wrxwhhqwlhuh hw oh sodirqg vl kdxw tx'rq duulydlw sdv d o'dshufhyrlu. Ghv wrufkhv hqiodpphhv hwdlhqwilahhv dxa pxuv gh slhuuh, frpph d Julqjrwwv, hw xq vrpswxhxa hvfdolhu gh pdueuh shuphwwdlwgh prqwhu gdqv ohv hwdjhv."
+    text = strip_puntuation(text)[0]
+    print(text)
+    print(len(caesar_decrypt_all(text)))
+    print(is_french(caesar_decrypt_all(text)))
